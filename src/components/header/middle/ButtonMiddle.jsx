0fe3a87171e7b23e25f07315eedfbb588cecc556
Ghuddy <mahmudtalal2@gmail.com>
2023-08-23 18:19:45 +0600

    add dynamic button

--- src/components/header/middle/ButtonMiddle.jsx
+++ src/components/header/middle/ButtonMiddle.jsx
@@ -12,7 +12,7 @@ const ButtonMiddle = () => {
       <div className="flex items-center  ">
         {/* <button className=" h[44px] w-[192px] flex justify-center items-center  text-base12 lg:text-base16 text-txt-secondary  rounded-[48px]">
         Check In
-      </button>
+      </button> 
       <div className="w-[1px] h-[35px] hidden md:block mx-8 bg-bg-primary"></div>
       <button className=" h[44px] w-[192px] text-base12 justify-center items-center  lg:text-base16 text-txt-secondary  rounded-[48px]">
         Check Out
